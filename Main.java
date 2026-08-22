f73cc9ca0fbd7d78e47f6b60c4370be10193b7eb
Faizan-Dev <mdshaanhassan09@gmail.com>
2026-08-23 02:05:20 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        TradingPlatform platform =
-                new TradingPlatform("Demo User", 10000.00);
-
-        platform.start();
-    }
-}
